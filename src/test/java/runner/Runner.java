303638a1338420ae0d761e34966ad06c7f5831c5
maliayyildiz <69386343+maliayyildiz@users.noreply.github.com>
2022-09-12 21:20:15 +0200

    first commit

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,16 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty","html:target/default-cucumber-report"},
+        features = "src/test/resources/features",
+        glue = "stepDefinitions",
+        tags = "@now",
+        dryRun = false
+)
+public class Runner {
+}
---
+++ src/test/java/stepDefinitions/Hooks.java
@@ -0,0 +1,46 @@
+package stepDefinitions;
+
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utils.Driver;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class Hooks {
+    //private bcz i ll use it only in this class
+    private AppiumDriverLocalService appiumServer = AppiumDriverLocalService.buildDefaultService();
+    final Runtime runtime = Runtime.getRuntime();
+    @Before
+    public void setUp(){
+        //it starts appium server
+        forceStopAppiumServer();
+
+        appiumServer.start();
+
+    }
+    @After
+    public void tearDown(Scenario scenario){
+        final byte[] screenshot=((TakesScreenshot) Driver.getAppiumDriver()).getScreenshotAs(OutputType.BYTES);
+        if (scenario.isFailed()) {
+            scenario.attach(screenshot, "image/png","screenshots");
+        }
+        Driver.quitAppiumDriver();
+        appiumServer.stop();
+
+    }
+
+    public void forceStopAppiumServer() {
+        try {
+            runtime.exec("killall node");
+            runtime.exec("pkill  -i xcodebuild");
+            System.out.println("Kill all nodes");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
